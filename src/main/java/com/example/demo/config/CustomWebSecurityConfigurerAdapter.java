f5a3c2924d7a0d5a0a743d4a3cbcef1245c75c5f
pavel.gorbunov <pavel.gorbunov@x5.ru>
2019-07-01 17:55:57 +0300

    master - add db auth

--- src/main/java/com/example/demo/config/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/example/demo/config/CustomWebSecurityConfigurerAdapter.java
@@ -1,8 +1,10 @@
 package com.example.demo.config;
 
+import com.example.demo.service.impl.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -27,38 +29,39 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userDetailsService);
+        auth.setPasswordEncoder(encodePWD());
+        return auth;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        System.out.println("configure!!!!!");
 //        auth.inMemoryAuthentication()
 //                .withUser("user").password("password").roles("USER")
 //                .and()
 //                .withUser("admin").password("$2a$04$9XYtm7RMnsx9WYZoeezlaOhSW63LbnSgmhUQ1MwOmfn1sXP5y9y2e").roles("ADMIN");
-        auth.userDetailsService(userDetailsService).passwordEncoder(encodePWD());
-    }
+//        auth.userDetailsService(userDetailsService).passwordEncoder(encodePWD());
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//
-//       // http.authorizeRequests().antMatchers("/register") .permitAll();
-//
-//        http.authorizeRequests().antMatchers("/register") .permitAll().
-//                and().
-//                authorizeRequests().antMatchers("/api/file").authenticated().anyRequest().permitAll()
-//                ;
-
-//        http.csrf().disable();
-//
-//        http.authorizeRequests().antMatchers("/register").authenticated().anyRequest().permitAll().and()
-//                .authorizeRequests().antMatchers("/api/**").authenticated().anyRequest().hasAnyRole("USER").and()
-//                .formLogin().permitAll();
 
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/register") .permitAll();
+
         http//.authorizeRequests().antMatchers("/api/**").authenticated().anyRequest().permitAll().and()
                 .authorizeRequests().antMatchers("/all").authenticated().anyRequest().hasAnyRole("USER").and()
                 .httpBasic();
-        http.authorizeRequests().antMatchers("/api/*").authenticated().and()
+        http.authorizeRequests().antMatchers("/api/*").authenticated().anyRequest().hasAnyRole("USER","ADMIN").and()
                 .httpBasic();
 
     }
